7bd005b6fcdfff5cabdff1047bb52dda0a1026ba
belkhadir <belkhadir.anas@gmail.com>
2023-09-08 16:31:05 +0100

    Initial Commit

---
+++ MovieCore/MovieCore.h
@@ -0,0 +1,18 @@
+//
+//  MovieCore.h
+//  MovieCore
+//
+//  Created by Belkhadir Anas on 8/9/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieCore.
+FOUNDATION_EXPORT double MovieCoreVersionNumber;
+
+//! Project version string for MovieCore.
+FOUNDATION_EXPORT const unsigned char MovieCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieCore/PublicHeader.h>
+
+

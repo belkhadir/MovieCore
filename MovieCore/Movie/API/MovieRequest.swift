cfde6eb26908724d57a9ee05d27787473c3e74a9
belkhadir <belkhadir.anas@gmail.com>
2023-09-22 15:50:55 +0100

    make the `MovieRequest` more flexible

--- MovieCore/Movie/API/MovieRequest.swift
+++ MovieCore/Movie/API/MovieRequest.swift
@@ -5,16 +5,30 @@
 //  Created by Belkhadir Anas on 16/9/2023.
 //
 
-public enum MovieRequest: String, Requestable {
-    case popular
-    case topRated = "top_rated"
-    case upcoming
+public struct MovieRequest: Requestable {
+    private let movieDiscover: MovieDiscover
+    private let page: Page
+    
+    public enum MovieDiscover: String {
+        case popular
+        case topRated = "top_rated"
+        case upcoming
+    }
+    
+    public init(movieDiscover: MovieDiscover = .popular, page: Page = Page(page: 1)) {
+        self.movieDiscover = movieDiscover
+        self.page = page
+    }
     
     public var path: String {
-        "/3/discover/" + rawValue
+        "/3/discover/" + movieDiscover.rawValue
     }
     
     public var requestType: RequestType {
         .GET
+    }
+    
+    public var urlParameters: [String : String?] {
+        ["page": "\(page.page)"]
     }
 }
---
+++ MovieCore/Shared/Domain/Page.swift
@@ -0,0 +1,14 @@
+//
+//  Page.swift
+//  MovieCore
+//
+//  Created by Belkhadir Anas on 22/9/2023.
+//
+
+public struct Page {
+    let page: Int
+    
+    public init(page: Int) {
+        self.page = page
+    }
+}
